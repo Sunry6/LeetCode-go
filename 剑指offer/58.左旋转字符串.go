868745a824bed4645bbdb746611af1f5d4ad8407
sunry <sryhardwork@gmail.com>
2022-01-04 15:38:29 +0800

    first commit

---
+++ 剑指offer/58.左旋转字符串.go
@@ -0,0 +1,16 @@
+package main
+
+func reverseLeftWords(s string, n int) string {
+	str1 := []byte(s)
+	len := len(str1)
+	var tmp []byte
+	for i := 0; i < n; i++ {
+		tmp = append(tmp, str1[i])
+	}
+	str1 = append(str1[n:len], tmp...)
+	return string(str1)
+}
+
+func main() {
+
+}

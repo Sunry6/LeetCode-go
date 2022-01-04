9576c06bb053e3cbbfe1b67f2cfaf7ff6376a3c9
sunry <sryhardwork@gmail.com>
2022-01-04 21:17:15 +0800

    剑指offer 03

---
+++ 剑指offer/03.findRepeatNumber.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// 方法一
+func findRepeatNumber(nums []int) int {
+	m := make(map[int]int)
+	for _, value := range nums {
+		if _, ok := m[value]; ok {
+			// 存在该数值
+			return value
+		} else {
+			//	不存在
+			m[value] = 1
+		}
+	}
+	return -1
+}
+
+// 方法二
+func findRepeatNumber2(nums []int) int {
+	sort.Ints(nums)
+
+	for i, numSize := 0, len(nums); i < numSize-1; i++ {
+		if nums[i] == nums[i+1] {
+			return nums[i]
+		}
+	}
+	return -1
+}
+
+func main() {
+	nums := []int{2, 3, 1, 0, 2, 5, 3}
+	num := findRepeatNumber(nums)
+	fmt.Println(num)
+
+	num2 := findRepeatNumber2(nums)
+	fmt.Println(num2)
+}

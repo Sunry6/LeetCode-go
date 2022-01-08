891b61f4574d82b87776087c437f9a5c7b457891
sunry <sryhardwork@gmail.com>
2022-01-08 22:45:55 +0800

    minArray

---
+++ 剑指offer/11.minArray.go
@@ -0,0 +1,35 @@
+package main
+
+// 153
+func findMin(number []int) int {
+	left, right := 0, len(number)-1
+	for left < right {
+		mid := left + (right-left)/2
+		if number[mid] < number[right] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return number[left]
+}
+
+// 154 && 11
+func findMin2(nums []int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] < nums[right] {
+			right = mid
+		} else if nums[mid] > nums[right] {
+			left = mid + 1
+		} else {
+			right--
+		}
+	}
+	return nums[left]
+}
+
+func main() {
+
+}

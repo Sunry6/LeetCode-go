ee97b88871e00956928b8eb093e0f4584a8fd9d6
sunry <sryhardwork@gmail.com>
2022-01-09 23:09:24 +0800

    50.firstUniqChar

---
+++ 剑指offer/50.firstUniqChar.go
@@ -0,0 +1,18 @@
+package main
+
+func firstUniqChar(s string) byte {
+	cnt := [26]int{}
+	for _, ch := range s {
+		cnt[ch-'a']++
+	}
+	for i, ch := range s {
+		if cnt[ch-'a'] == 1 {
+			return s[i]
+		}
+	}
+	return ' '
+}
+
+func main() {
+
+}

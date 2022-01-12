a2775567f90a6f47119507f0ac800fdecab62ddd
sunry <sryhardwork@gmail.com>
2022-01-12 23:06:46 +0800

    27.mirrorTree

---
+++ 剑指offer/27.mirrorTree.go
@@ -0,0 +1,16 @@
+package main
+
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	left := mirrorTree(root.Left)
+	right := mirrorTree(root.Right)
+	root.Left = right
+	root.Right = left
+	return root
+}
+
+func main() {
+
+}
